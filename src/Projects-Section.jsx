30ac27fc0e068ed3c639f0a8482e56202efb5851
Anthony <isijolaayomikun04@gmail.com>
2025-10-26 01:07:52 -0700

    lit twerks

--- src/Projects-Section.jsx
+++ src/Projects-Section.jsx
@@ -30,87 +30,87 @@ function ProjectsSection () {
 
       <p className="mt-6 ml-4 text-2xl text-center font-extrabold text-black">Here are my Dev Experiments.</p>
 
-      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[650px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
+      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[700px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
         <div className="mx-auto rounded-2xl border-gray-300 border w-5/6 h-1/3 overflow-hidden">
           <img className="w-full h-full object-cover" src="package.png" />
         </div>
         <p className="ml-6 mr-6 text-xl font-bold text-black">Universities in Nigeria - NPM Package</p>
         <p className="ml-6 text-base font-medium text-gray-600">An NPM package that provides a complete and up-to-date list of all federal, state, and private universities in Nigeria for easy integration into web and data applications.</p>
         <div>
-          <button className="ml-6 h-10 w-28 text-white bg-black rounded-lg font-medium text-base">JavaScript</button>
-          <button className="ml-2 h-10 w-32 text-white bg-black rounded-lg font-medium text-base">Node.js(basics)</button>
+          <button className="ml-6 h-10 w-28 text-white bg-gray-700 rounded-lg font-medium text-base">JavaScript</button>
+          <button className="ml-2 h-10 w-32 text-white bg-gray-700 rounded-lg font-medium text-base">Node.js(basics)</button>
         </div>
         <button className="ml-6 h-[50px] w-48 bg-gray-400 rounded-lg font-medium text-base">View Project on NPM</button>
         <button className="ml-6 h-[50px] w-56 bg-gray-400 rounded-lg font-medium text-base">View Project on Github</button>
       </div>
 
-      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[650px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
+      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[700px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
         <div className="mx-auto rounded-2xl border-gray-300 border w-5/6 h-1/3 overflow-hidden">
           <img className="w-full h-full object-cover" src="inerface.png" />
         </div>
         <p className="ml-6 mr-6 text-xl font-bold text-black">Nigerian Universities - Demo Interface</p>
         <p className="ml-6 text-base font-medium text-gray-600">A quick sneak peak into how the Nigerian Universities package works - clean UI, simple data display and smooth experience.</p>
         <div>
-          <button className="ml-6 h-10 w-28 text-white bg-black rounded-lg font-medium text-base">TailwindCSS</button>
-          <button className="ml-2 h-10 w-20 text-white bg-black rounded-lg font-medium text-base">React.js</button>
+          <button className="ml-6 h-10 w-28 text-white bg-gray-700 rounded-lg font-medium text-base">TailwindCSS</button>
+          <button className="ml-2 h-10 w-20 text-white bg-gray-700 rounded-lg font-medium text-base">React.js</button>
         </div>
         <button className="ml-6 h-[50px] w-48 bg-gray-400 rounded-lg font-medium text-base">Live Demo</button>
         <button className="ml-6 h-[50px] w-56 bg-gray-400 rounded-lg font-medium text-base">View Project on Github</button>
       </div>
 
-      <div className="flex flex-col justify-evenly mt-6 mx-auto h-[650px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
+      <div className="flex flex-col justify-evenly mt-6 mx-auto h-[700px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
         <div className="rounded-xl mx-auto border-gray-300 border w-5/6 h-1/3 overflow-hidden">
           <img className="w-full h-full object-cover" src="third.png" />
         </div>
         <p className="ml-6 text-xl font-bold text-black">GreenLab</p>
         <p className="ml-6 text-base font-medium text-gray-600">Building a mobile web app that allows users to identify households chemicals, understand safe usage and promotes eco-friendly alternatives.</p>
         <div>
-          <button className="ml-6 h-10 w-28 text-white bg-black rounded-lg font-medium text-base">TailwindCSS</button>
-          <button className="ml-2 h-10 w-20 text-white bg-black rounded-lg font-medium text-base">React.js</button>
+          <button className="ml-6 h-10 w-28 text-white bg-gray-700 rounded-lg font-medium text-base">TailwindCSS</button>
+          <button className="ml-2 h-10 w-20 text-white bg-gray-700 rounded-lg font-medium text-base">React.js</button>
         </div>
         <button className="ml-6 h-[50px] w-48 bg-gray-400 rounded-lg font-medium text-base">Live Demo</button>
         <button className="ml-6 h-[50px] w-56 bg-gray-400 rounded-lg font-medium text-base">View Project on Github</button>
       </div>
 
-      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[650px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
+      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[700px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
         <div className="mx-auto rounded-xl border-gray-300 border w-5/6 h-1/3 overflow-hidden">
           <img className="w-full h-full object-cover" src="fourth.png" />
         </div>
         <p className="ml-6 text-xl font-bold text-black">ChemQuest</p>
         <p className="ml-6 text-base font-medium text-gray-600">An interactive quiz game that helps users learn about different chemicals, their use, and their effects on the environment.</p>
         <div>
-          <button className="ml-6 h-10 w-28 text-white bg-black rounded-lg font-medium text-base">TailwindCSS</button>
-          <button className="ml-2 h-10 w-24 text-white bg-black rounded-lg font-medium text-base">JavaScript</button>
+          <button className="ml-6 h-10 w-28 text-white bg-gray-700 rounded-lg font-medium text-base">TailwindCSS</button>
+          <button className="ml-2 h-10 w-24 text-white bg-gray-700 rounded-lg font-medium text-base">JavaScript</button>
         </div>
         <button className="ml-6 h-[50px] w-48 bg-gray-400 rounded-lg font-medium text-base">Live Demo</button>
         <button className="ml-6 h-[50px] w-56 bg-gray-400 rounded-lg font-medium text-base">View Project on Github</button>
       </div>
 
-      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[650px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
+      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[700px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
         <div className="mx-auto rounded-xl border-gray-300 border w-5/6 h-1/3 overflow-hidden">
           <img className="w-full h-full object-cover" src="first.png" />
         </div>
         <p className="ml-6 text-xl font-bold text-black">An E-commerce Website</p>
         <p className="ml-6 text-base font-medium text-gray-600">Working on a responsive e-commerce website with React.js and Tailwind CSS, with ongoing work to make it fully interactive.</p>
         <div>
-          <button className="ml-6 h-10 w-16 text-white bg-black rounded-lg font-medium text-base">CSS</button>
-          <button className="ml-2 h-10 w-24 text-white bg-black rounded-lg font-medium text-base">JavaScript</button>
-          <button className="ml-2 h-10 w-20 text-white bg-black rounded-lg font-medium text-base">React.js</button>
+          <button className="ml-6 h-10 w-16 text-white bg-gray-700 rounded-lg font-medium text-base">CSS</button>
+          <button className="ml-2 h-10 w-24 text-white bg-gray-700 rounded-lg font-medium text-base">JavaScript</button>
+          <button className="ml-2 h-10 w-20 text-white bg-gray-700 rounded-lg font-medium text-base">React.js</button>
         </div>
         <button className="ml-6 h-[50px] w-48 bg-gray-400 rounded-lg font-medium text-base">Live Demo</button>
         <button className="ml-6 h-[50px] w-56 bg-gray-400 rounded-lg font-medium text-base">View Project on Github</button>
       </div>
 
-      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[650px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
+      <div className="flex flex-col justify-evenly mt-10 mx-auto h-[700px] w-[85vw] border-gray-300 border rounded-2xl shadow-lg">
         <div className="mx-auto rounded-2xl border-gray-300 border w-5/6 h-1/3 overflow-hidden">
           <img className="w-full h-full object-cover" src="second.png" />
         </div>
         <p className="ml-6 text-xl font-bold text-black">A ChatBot</p>
         <p className="ml-6 text-base font-medium text-gray-600">Built a simple chatbot with React.js that answers user questions in real time, working on adding more data to it.</p>
         <div>
-          <button className="ml-6 h-10 w-16 text-white bg-black rounded-lg font-medium text-base">CSS</button>
-          <button className="ml-2 h-10 w-24 text-white bg-black rounded-lg font-medium text-base">JavaScript</button>
-          <button className="ml-2 h-10 w-20 text-white bg-black rounded-lg font-medium text-base">React.js</button>
+          <button className="ml-6 h-10 w-16 text-white bg-gray-700 rounded-lg font-medium text-base">CSS</button>
+          <button className="ml-2 h-10 w-24 text-white bg-gray-700 rounded-lg font-medium text-base">JavaScript</button>
+          <button className="ml-2 h-10 w-20 text-white bg-gray-700 rounded-lg font-medium text-base">React.js</button>
         </div>
         <button className="ml-6 h-[50px] w-48 bg-gray-400 rounded-lg font-medium text-base">Live Demo</button>
         <button className="ml-6 h-[50px] w-56 bg-gray-400 rounded-lg font-medium text-base">View Project on Github</button>
